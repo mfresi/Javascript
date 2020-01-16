0d683bbcb18253241b56f72a9426ea81a3408749
Mattei Fresi <mfresi@la-providence.net>
2020-01-16 16:34:09 +0100

    commit_exo_15

---
+++ Exercice15.js
@@ -0,0 +1,9 @@
+function init(){
+
+    var monobjet = addEventListener("click", gestionnaire, false); 
+}
+
+function gestionnaire(){
+
+    document.getElementById('texte').style.backgroundColor = 'orange';
+}
